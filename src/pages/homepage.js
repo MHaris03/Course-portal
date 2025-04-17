3d8a5fcad296bd6f0a033d06e4850e744d22a2ac
MHaris03 <xhkharis2@gmail.com>
2025-04-17 16:14:20 +0500

    logo

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -3,7 +3,6 @@ import { useNavigate, useLocation } from 'react-router-dom';
 import '../style/Homepage.css';
 import TopClasses from './TopClasses';
 import toast, { Toaster } from 'react-hot-toast';
-import { Helmet } from 'react-helmet';
 
 function Homepage() {
   const navigate = useNavigate();
@@ -33,23 +32,6 @@ function Homepage() {
 
   return (
     <div className="app">
-      <Helmet>
-        <title>UGA Course Genie | Find Your Ideal Classes</title>
-        <meta name="description" content="Explore and review UGA courses easily." />
-
-        {/* Open Graph meta tags */}
-        <meta property="og:title" content="UGA Course Genie" />
-        <meta property="og:description" content="Explore and rate UGA courses with ease." />
-        <meta property="og:image" content="https://res.cloudinary.com/dfs0l1ady/image/upload/v1744886693/logo_sddghl.png" />
-        <meta property="og:url" content="https://res.cloudinary.com/dfs0l1ady/image/upload/v1744886693/logo_sddghl.png" />
-        <meta property="og:type" content="website" />
-
-        {/* Twitter Card meta tags */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="UGA Course Genie" />
-        <meta name="twitter:description" content="Explore and rate UGA courses with ease." />
-        <meta name="twitter:image" content="https://res.cloudinary.com/dfs0l1ady/image/upload/v1744886693/logo_sddghl.png" />
-      </Helmet>
       <main className="main-content">
         <Toaster />
         <section className="intro">
